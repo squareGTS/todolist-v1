18ea7db17cae31556ab815d05cb497a90b96f592
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-10-20 20:59:53 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public")); // can use css
+app.get("/", function(req, res) {
+
+const day = date.getDate();
+
+
+  // var currentDay = today.getDay();
+  // var day = "";
+  //
+  // switch (currentDay) {
+  //   case 0:
+  //     day = "Sunday";
+  //     break;
+  //   case 1:
+  //     day = "Monday";
+  //     break;
+  //   case 2:
+  //     day = "Tuesday";
+  //     break;
+  //   case 3:
+  //     day = "Wednesday";
+  //     break;
+  //   case 4:
+  //     day = "Thursday";
+  //     break;
+  //   case 5:
+  //     day = "Friday";
+  //     break;
+  //   case 6:
+  //     day = "Saturday";
+  //     break;
+  //   default:
+  //     console.log("Error: current day is equal to:" + currentDay);
+  // }
+
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
+
+
+  app.post("/", function(req, res) {
+
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
+  });
+
+  app.post("/work", function() {
+    const item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+  });
+
+  app.get("/work", function(req, res) {
+    res.render("list", {
+      listTitle: "Work List",
+      newListItems: workItems
+    });
+  });
+
+  app.get("/about", function(req, res) {
+    res.render("about");
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
